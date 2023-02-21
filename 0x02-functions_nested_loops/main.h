2724c118e1180ef40fcd3fb036b8f297417152e4
dagiLM10 <dagiberhanu19@gmail.com>
2023-02-21 05:16:15 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sig(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_tablet(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_tablet(int n);
+
+#endif
