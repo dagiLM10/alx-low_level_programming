26398aaaee3503a7fed07f68ca93a53b2994d6c4
dagiLM10 <dagiberhanu19@gmail.com>
2023-03-02 10:13:27 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
-char *_strcat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
@@ -11,7 +11,7 @@ char *string_toupper(char *a);
 char *leet(char *a);
 char *rot13(char *a);
 void print_number(int n);
-char *infinite_add(char *n1, char *2, char *r, int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
 #endif
