fbe6a1a64558f0459f0ded2ce9e03f52fc7ec19a
dagiLM10 <dagiberhanu19@gmail.com>
2023-03-08 10:57:02 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,7 +5,7 @@ int check_prime(int n, int i);
 /**
  * is_prime_number - Returns if a number is prime
  * @n: the number to be check
- * Return: integer value 
+ * Return: integer value
  */
 int is_prime_number(int n)
 {
