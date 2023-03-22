bb31f3e3f5d2cac89f3e9f1f39dae39d04c61580
dagiLM10 <dagiberhanu19@gmail.com>
2023-03-22 03:08:00 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-#include "fuction_pointers.h"
+#include "function_pointers.h"
 
 /**
  * int_index - searches for an integer
@@ -18,10 +18,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(array[i] != 0)
+			if (cmp(array[i] != 0))
 				return (i);
 		}
 	}
 
 	return (-1);
-	}	
+	}
