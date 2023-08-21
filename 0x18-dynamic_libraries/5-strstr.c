2e9d91745099e0b268440453a40a3d2cdd07299a
dagiLM10 <dagiberhanu19@gmail.com>
2023-08-21 03:30:28 -0700

    dynamic

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring.
+ * @haystack: The string to be searched.
+ * @needle: The substring to be located.
+ *
+ * Return: If the substring is located - a pointer to the beginning
+ *					of the located substring.
+ *		If the substring is located  - NULL.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *one = haystack;
+		char *two = needle;
+
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
+		}
+
+		if (*two == '\0')
+			return (haystack);
+	}
+	return (NULL);
+}
