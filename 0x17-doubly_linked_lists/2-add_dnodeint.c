5ff9d48a72cede627f9ff72d30ca0e3236e9cbfe
dagiLM10 <dagiberhanu19@gmail.com>
2024-01-11 07:39:54 -0800

    ./c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -24,13 +24,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (h != NULL)
 	{
 		while (h->prev != NULL)
-			h = h -> prev;
+			h = h->prev;
 	}
 
 	new->next=h;
 
 	if (h != NULL)
-		h -> prev = new;
+		h->prev = new;
 
 	*head = new;
 
