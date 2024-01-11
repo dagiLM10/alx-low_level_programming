65d82ce4d64529908c43cf4f239faa62534e3209
dagiLM10 <dagiberhanu19@gmail.com>
2024-01-11 07:29:05 -0800

    print

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,28 +1,31 @@
 #include "lists.h"
 
 /**
-*print_dlistint - prints all the elements of a
-*dlistint_t list
-*
-*@h: head of the list
-*Return: the number of nodes
-*/
-size_t print_dlistint_rec(const dlistint_t *h, size_t elements)
-{
-	if (h)
-	{
-		printf("%i\n", h->n);
-		elements = print_dlistint_rec(h->next, elements + 1);
-	}
-	return (elements);
-}
-
-/**
- * print_dlistint - prints all the elements of a dlistint_t list.
- * @h: double linked list.
- * Return: number of nodes.
+ * print_dlistint - prints all the elements of a
+ * dlistsint_t list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	return (print_dlistint_rec(h, 0));
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
+
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,19 +1,19 @@
 #ifndef _LISTS_
-#define _Lists_
+#define _LISTS_
 
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
-*struct dlistint_s - doubly linked list
-*@n: integer
-*@prev: points to the previous node
-*@next: points to the next node
-*
-*Description: doubly linked list node structure
-*for Holberton project
-*/
-typedef struct dlistint_d
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct dlistint_s
 {
 	int n;
 	struct dlistint_s *prev;
